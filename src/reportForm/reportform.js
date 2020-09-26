5d873c04bca9e71daa08e576063f6b8c05e2b2ca
Jacob Springborn <jospring@buffalo.edu>
2020-09-26 14:45:25 -0400

    Commit  report form and for errors and updating info

---
+++ src/reportForm/reportform.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ReportForm extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      error: '',
+      page_found_on: '',
+      details: '',
+      correction: ''
+    };
+  }
+
+  handleChange = (event) => {
+    let name_ = event.target.name;
+    let value_ = event.target.value;
+    this.setState({[name_]: value_});
+  }
+
+  handleSubmit = (event) => {
+    alert('Report submitted successfully');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+     <form onSubmit={this.handleSubmit}>
+       <br/>
+       <h1>Report Form</h1>
+      <div className="container">
+        <div className="row">
+          <div classname="col-25">
+            <label>Error: </label>
+          </div>
+          <div className="col-85">
+            <input
+            type='text'
+            name='error'
+            onChange={this.handleChange}
+            />
+          </div> 
+        </div>
+          <div className="row">
+            <div classname="col-25">
+              <label>Page found on: </label>
+            </div>
+            <div className="col-85">
+              <input
+              type='text'
+              name='page_found_on'
+              onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div classname="col-25">
+              <label>Details of the error: </label>
+            </div>
+              <div className="col-85">
+                <input
+                type='text'
+                name='details'
+                onChange={this.handleChange}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div classname="col-25">
+                <label>Correction: </label>
+              </div>
+              <div className="col-85">
+                <input
+                type='text'
+                name='correction'
+                onChange={this.handleChange}
+                />
+            </div>
+          </div>
+
+          <br/>
+          <div className="row">
+          <input type='submit' />
+          </div>
+
+      </div>
+     </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <ReportForm />,
+  document.getElementById('root')
+);
