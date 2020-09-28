e8908af967be794b546521b1bba51ac9517c0a3a
Muhtasim Maruf <muhtasimz@gmail.com>
2020-09-27 20:48:26 -0400

    Delete cloneNode.js

--- cloneNode.js
+++
@@ -1,7 +0,0 @@
-function duplicate() {
-    var original = document.getElementById('duplicater' + i);
-    var clone = original.cloneNode(true); 
-    clone.id = "duplicetor" + ++i; 
-    clone.onclick = duplicate; 
-    original.parentNode.appendChild(clone);
-}
