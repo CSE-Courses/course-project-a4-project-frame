aef9b28a003f1926c0b6bb521b2a0e58e341d657
JustinKozlowski <jjkozlow@buffalo.edu>
2020-10-18 12:35:44 -0400

    Updated Games to call API to render Components. Issue #39

--- src/Games/Games.js
+++ src/Games/Games.js
@@ -4,11 +4,41 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Game from './Game';
 import Modal from '../Modal/Modal';
 import '../Modal/ModalScripts.js';
+import "../../serverIP";
 
 
 export default class Games extends Component {
-    getGames() {
-        var games = ['Ultimate', 'Tekken', 'DBZ'];
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: [] //Will be used to store all characters for the game
+        };
+      }
+    componentDidMount() {
+        fetch("http://" + serverIP + "/get/games") // Calls server for characters to fill the tiles currently set for development server
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                isLoaded: true,
+                items: result
+              });
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+              this.setState({
+                isLoaded: true,
+                error
+              });
+            }
+        )
+    }
+      getGames() {
+        var games = this.state.items;
         const gamesList = games.map((game) => <Game key={game} game={game} />);
         return gamesList;
     }
