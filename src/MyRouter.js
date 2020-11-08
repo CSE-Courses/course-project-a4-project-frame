51627ee7b0f02ec46a731e58cbe8d17d91907a48
Jacob Springborn <jospring@buffalo.edu>
2020-11-08 17:59:55 -0500

    Revert "Jacob springborn sprint3 scenario form"

--- src/MyRouter.js
+++ src/MyRouter.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Games from './Games/Games.js';
 import Characters from './Characters/Characters.js';
 import Attacks from './Attacks/Attacks'
-import Scenarios from './Unique/Scenarios'
 
 export default function RouterPage() {
     return (
@@ -14,8 +13,7 @@ export default function RouterPage() {
             </Link>
             <Route component={Games} exact path="/" />
             <Route component={Characters} exact path="/:game" />
-            <Route component={Attacks} exact path ="/:game/:character" />
-            <Route component={Scenarios} exact path ="/:game/:character/Scenarios" />
+            <Route component={Attacks} path ="/:game/:character" />
             
         </div>
     )
