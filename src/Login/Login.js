5cceb7699c3e67f041ac11b59af3e2870496f0d4
Muhtasim Maruf <muhtasimz@gmail.com>
2020-11-22 15:25:04 -0500

    Login Page #70
    Login Page

---
+++ src/Login/Login.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+
+import {Button, Form, FormGroup, Label, Input}
+ from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+    <Form className="login-form">
+     <h1>
+       <span className="center">Project Frame</span>
+       </h1>
+       <h2>Start Fighting!</h2>
+       <FormGroup>
+         <Label>Username</Label>
+         <Input type="text" placeholder ="Username"></Input>
+       </FormGroup>
+       <FormGroup>
+         <Label>Password</Label>
+         <Input type="text" placeholder ="Password"></Input>
+       </FormGroup>
+       <Button className = "btn-lg btn-dark btn-block">
+         Log In
+       </Button>
+       <div className = "text-center">
+         <a href="/sign up">Sign Up</a>
+       </div>
+    </Form>
+  );
+}
+}
+
+export default App;
