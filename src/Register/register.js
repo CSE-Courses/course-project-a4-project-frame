dd4f695ee8e8852d72e36289edf4e9734dd08d65
Bangyu Li <bangyuli@buffalo.edu>
2020-11-30 07:25:35 +0800

    Issue #92
    Issue #92 reate register page for user registration

---
+++ src/Register/register.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { useLocation } from 'react-router-dom';
+import './register.css';
+import serverIP from '../serverIP';
+
+export default function Register() {
+    const location = useLocation();
+    return (
+        <div>
+            <CharacterForm location={location.pathname}/>
+        </div>
+    )
+}
+
+class CharacterForm extends React.Component {
+	
+	render() {
+		return (
+			<form method="POST" action={"http://" + serverIP["serverIP"] + this.props.location +"/register"} enctype="multipart/form-data">
+				<br/>
+				<h1>Sign up</h1>
+				<div className="container">
+					<div className="row">
+						<div className="col-25">
+							<label>Username</label>
+						</div>
+						<div className="col-85">
+							<input
+								type='text'
+								name='username'
+							/>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-25">
+							<label>Password</label>
+						</div>
+						<div className="col-85">
+							<input
+								type='password'
+								name='password'
+							/>
+						</div>
+					</div>
+                    <div className="row">
+						<div className="col-25">
+							<label>Email</label>
+						</div>
+						<div className="col-85">
+							<input
+								type='text'
+								name='email'
+							/>
+						</div>
+					</div>
+					<br/>
+					<div className="row">
+						<input type='submit' />
+					</div>
+				</div>
+			</form>
+		);
+	}
+}
